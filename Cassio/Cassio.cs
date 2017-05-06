25f5bb140ce14d6415788adc572d3840f804d67b
Fabian Brand <fabian@fabianbrand.de>
2017-05-06 21:28:11 +0200

    Added ultimate usage and rough performance adjustments

--- Cassio/Cassio.cs
+++ Cassio/Cassio.cs
@@ -28,18 +28,25 @@ namespace Cassio
         {
             Chat.Print("Loading Cassiopeia");
             _rootMenu = Menu.AddMenu("Cassiopeia");
+            _rootMenu.Add(new MenuSlider("limitOnTick", "Limit ticks (Higher limit has better performance)",
+                0, 30, 0));
             var comboMenu = _rootMenu.AddSubMenu("Combo");
             var laneClearAndLasthitMenu = _rootMenu.AddSubMenu("Laneclear & Lasthit");
             comboMenu.Add(new MenuCheckbox("useQCombo", "Use Q in Combo"));
             comboMenu.Add(new MenuCheckbox("useWCombo", "Use W in Combo"));
             comboMenu.Add(new MenuCheckbox("useECombo", "Use E in Combo"));
             comboMenu.Add(new MenuCheckbox("useRCombo", "Use R in Combo"));
+            comboMenu.Add(new MenuSlider("useRMinimum", "Use R if it will stun atleast", 0, 5, 2));
             comboMenu.Add(new MenuCheckbox("focusPoisioned", "Focus poisioned enemies"));
             laneClearAndLasthitMenu.Add(new MenuCheckbox("useELasthit", "Use E in Laneclears"));
         }
 
         private void GameOnOnTick()
         {
+            Utilities.Tick();
+            var targetTicks = _rootMenu.Get<MenuSlider>("limitOnTick").CurrentValue;
+            if (!Utilities.ShouldWork(targetTicks)) return;
+            //Check if anyone is killable with a single e and try to get the kill
             var killableTarget = Utilities.GetKillableTarget(_e);
             if (killableTarget != null && _e.CanCast(killableTarget))
             {
@@ -53,28 +60,38 @@ namespace Cassio
                 Combo();
             }
             else if (firstOrDefault != null && (firstOrDefault
-                         .ActiveMode.HasFlag(Orbwalker.OrbwalkingMode.LaneClear) ||
-                         firstOrDefault.ActiveMode.HasFlag(Orbwalker.OrbwalkingMode.LastHit)))
+                                                    .ActiveMode.HasFlag(Orbwalker.OrbwalkingMode.LaneClear) ||
+                                                firstOrDefault.ActiveMode.HasFlag(Orbwalker.OrbwalkingMode
+                                                    .LastHit)))
             {
                 LaneClear();
             }
-
         }
 
         private void LaneClear()
         {
+
             var target = Utilities.GetLastHittableMinion(_e);
-            if (target != null)
+            if (target == null) return;
+            if (_e.CanCast(target))
             {
-                if (_e.CanCast(target))
-                {
-                    _e.CastOnUnit(target);
-                }
+                _e.CastOnUnit(target);
             }
         }
 
         private void Combo()
         {
+
+            //Check if you can ult targets
+            foreach (var hero in ObjectManager.Heroes.Enemies)
+            {
+                if (_r.IsReady() && Utilities.WillStunMinimum(_r,
+                        _rootMenu.SubMenu("Combo").Get<MenuSlider>("useRMinimum").CurrentValue, hero.Position))
+                {
+                    _r.Cast(hero);
+                }
+            }
+
             if (_rootMenu.SubMenu("Combo").Get<MenuCheckbox>("focusPoisioned").Checked)
                             {
                                 if (_e.IsReady() && _rootMenu.SubMenu("Combo").Get<MenuCheckbox>("useECombo").Checked)
--- Cassio/Utilities.cs
+++ Cassio/Utilities.cs
@@ -1,15 +1,43 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using HesaEngine.SDK;
 using HesaEngine.SDK.Enums;
 using HesaEngine.SDK.GameObjects;
+using SharpDX;
 
 namespace Cassio
 {
     public class Utilities
     {
+        private static int _currentTick = 0;
+
+        public static void Tick()
+        {
+            _currentTick++;
+        }
+
+        public static bool ShouldWork(int targetValue)
+        {
+            if (targetValue <= _currentTick)
+            {
+                _currentTick = 0;
+                return true;
+            }
+            return false;
+        }
+
+        public static bool WillStunMinimum(Spell spell, int count, Vector3 target)
+        {
+            //Does not work as intended yet, need to count the actual facing targets
+            return count <= spell.CountHits(ObjectManager.Heroes.Enemies.ToList<Obj_AI_Base>(), target)
+                   && ObjectManager.Heroes.Enemies.Count(enemy => enemy.IsFacing(ObjectManager.Player)
+                                                                  && enemy.Distance(ObjectManager.Player) <= spell.Range)
+                   >= count;
+        }
+
         public static AIHeroClient GetKillableTarget(Spell spell) => ObjectManager.Heroes.Enemies
             .FirstOrDefault(enemy => enemy.Distance(ObjectManager.Player) < spell.Range
-                                     && enemy.Health < spell.GetDamage(enemy));
+                                     && enemy.Health < spell.GetDamage(enemy) && !enemy.IsInvulnerable);
 
         public static Obj_AI_Minion GetLastHittableMinion(Spell spell) => ObjectManager.MinionsAndMonsters.Enemy
             .FirstOrDefault(minion => minion.Distance(ObjectManager.Player) < spell.Range &&
@@ -17,21 +45,17 @@ namespace Cassio
 
         public static void CheckAndCastSpellAoe(Spell spell)
         {
-            if (spell.IsReady())
-            {
-                var target = TargetSelector.GetTarget(spell.Range, TargetSelector.DamageType.Magical,
-                    false);
-                spell.Cast(target, aoe:true);
-            }
+            if (!spell.IsReady()) return;
+            var target = TargetSelector.GetTarget(spell.Range, TargetSelector.DamageType.Magical,
+                false);
+            spell.Cast(target, aoe:true);
         }
         public static void CheckAndCastSpellTargeted(Spell spell)
         {
-            if (spell.IsReady())
-            {
-                var target = TargetSelector.GetTarget(spell.Range, TargetSelector.DamageType.Magical,
-                    false);
-                spell.CastOnUnit(target);
-            }
+            if (!spell.IsReady()) return;
+            var target = TargetSelector.GetTarget(spell.Range, TargetSelector.DamageType.Magical,
+                false);
+            spell.CastOnUnit(target);
         }
 
         public static bool PoisonConditions(AIHeroClient aiHeroClient)
